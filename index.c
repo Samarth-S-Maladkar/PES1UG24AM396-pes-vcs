0a54af1383a592d41cd130d97dced7720dab7656
Samarth-S-Maladkar <SAMARTH0509@GMAIL.COM>
2026-04-17 08:05:15 +0530

    Phase3:Remove index_find function implementation
    Removed the implementation of the index_find function.

--- index.c
+++ index.c
@@ -28,11 +28,7 @@
 
 // Find an index entry by path (linear scan).
 IndexEntry* index_find(Index *index, const char *path) {
-    for (int i = 0; i < index->count; i++) {
-        if (strcmp(index->entries[i].path, path) == 0)
-            return &index->entries[i];
-    }
-    return NULL;
+   
 }
 
 // Remove a file from the index.
